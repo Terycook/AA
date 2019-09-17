df8de69244195eecf74dbb56686e29c291b703b0
Tery Cook <cook48@nsuok.edu>
2019-09-16 22:00:30 -0500

    Initial commit

---
+++ Assets/spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    public GameObject pinPrefab;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin()
+    {
+        Instantiate(pinPrefab, transform.position, transform.rotation);
+    }
+
+}
